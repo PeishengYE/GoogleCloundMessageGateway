f402d665024ed6c1dc9fbda19a15b90253f65910
Peisheng YE <peisheng.ye.88@gmail.com>
2016-10-10 19:43:35 -0400

    added new constants

--- app/src/main/java/com/radioyps/gcm_test/CommonConstants.java
+++ app/src/main/java/com/radioyps/gcm_test/CommonConstants.java
@@ -5,7 +5,8 @@ package com.radioyps.gcm_test;
  */
 public class CommonConstants {
     public static int connectPort = 5028;
-    public static final String IP_ADDR_DOOR_CONTROLLER = "192.168.12.240";
+    //public static final String IP_ADDR_DOOR_CONTROLLER = "192.168.12.240";
+    public static final String IP_ADDR_DOOR_CONTROLLER = "192.168.12.238";
     public final static int SOCKET_TIMEOUT = 10 * 000; /*10 seconds */
     public static final String GCM_authrized_mesg = "ds`133*(/.Mnd";
     public static String CMD_PRESS_DOOR_BUTTON = "A412..&35?@!";
@@ -13,6 +14,12 @@ public class CommonConstants {
 
     public static final String CMD_PING_CONTROLLER = "78*(^@/uid";
     public static final String PING_ACK = "^3234adsfa/?";
+    public static final String ACTION_PING = "com.radioyps.gcm_test.doorcontroller.ACTION_PING";
+    public static final String ACTION_PRESS_DOOR_BUTTON = "com.radioyps.gcm_test.doorcontroller.ACTION_PRESS_DOOR_BUTTON";
+    public static final String ACTION_PRESS_REMOTE_BUTTON = "com.radioyps.gcm_test.doorcontroller.ACTION_PRESS_REMOTE_BUTTON";
+    public static final String ACTION_STOP_CONNECTING = "com.radioyps.gcm_test.doorcontroller.ACTION_STOP_CONNECTING";
+    public static final String ACTION_START_CONNECTING = "com.radioyps.gcm_test.doorcontroller.ACTION_START_CONNECTING";
+
 
 
 }
